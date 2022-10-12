1aae00d4f399da7af0145d44a52cfa34791ce5a9
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-12 17:39:49 -0500

    Create methods for summarize_tree_ftw.c that pass all bats tests and have no memory leaks

--- summarize_tree/summarize_tree_ftw.c
+++ summarize_tree/summarize_tree_ftw.c
@@ -1,21 +1,36 @@
 #include <stdio.h>
-#include <sys/stat.h>
-#include <stdbool.h>
-#include <stdlib.h>
-#include <dirent.h>
-#include <unistd.h>
-#include <string.h>
+#include <ftw.h>
+
+static int num_dirs, num_regular;
 
 static int callback(const char *fpath, const struct stat *sb, int typeflag) {
-    // Define stuff here
+    // If the given entry is a directory, increase the number of directories
+    if(S_ISDIR(sb->st_mode)) {
+        num_dirs++;
+    } else {
+    // Otherwise, increase the number of files
+        num_regular++;
+    }
+    return 0;
 }
 
 #define MAX_FTW_DEPTH 16
 
 int main(int argc, char** argv) {
     // Check arguments and set things up
+    if (argc != 2) {
+        printf("ERROR: Enter program name and directory; Two argumetns expected!");
+	return 1;
+    }
 
+    // Set the number of directories and files to zero
+    num_dirs = 0;
+    num_regular = 0;
+
+    // Count the number of directories and files
     ftw(argv[1], callback, MAX_FTW_DEPTH);
 
     // Print out the results
+    printf("There were %d directories.\n", num_dirs);
+    printf("There were %d regular files.\n", num_regular);
 }
